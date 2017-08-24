4d922e28cb6650c6d70519d1777119638012362f
kanishka <nish2575@gmail.com>
2017-08-24 18:14:33 -0400

    makeHasFor TH

--- has-pattern/src/Lib3.hs
+++ has-pattern/src/Lib3.hs
@@ -3,6 +3,7 @@ module Lib3
 where
 
 import Lib3th
+import qualified Language.Haskell.TH as TH
 
 someFunc :: IO ()
 someFunc =
@@ -11,12 +12,23 @@ someFunc =
 data Record2 = Record2
   { field1 :: Char  -- why does this cause an error when field1 is an Int??
   , field2 :: String
+  , field3 :: SubRecord
   }
 
+data SubRecord = SubRecord
+  { subfield1 :: ValType
+  }
+
+
+
+data ValType = ValType
+
 makeHasRec ''Record2 ['field1, 'field2]
+makeHasRec ''SubRecord ['subfield1]
 
 -- getFieldType ''Record2 'field1  -- decide how to turn "type" into [declaration]
 
 makeHasFor ''Record2 []
 
--- makeHasFor ''Record2 [('field1, .., [...])]
+ -- this needs Has SubRecord ValType... a little unclear on what is going on here
+makeHasFor ''Record2 [('field3, TH.ConT ''SubRecord, [TH.ConT ''ValType])]
